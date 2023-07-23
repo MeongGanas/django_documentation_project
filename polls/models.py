a2748f37df45a7806c5d42aa0c84f07add52bca3
MeongGanas <farreluken@gmail.com>
2023-07-23 19:44:32 +0800

    salah nama

--- polls/models.py
+++ polls/models.py
@@ -28,13 +28,13 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-class Choise(models.Model):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choise_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return str(self.choise_text)
+        return str(self.choice_text)
     
 
 
