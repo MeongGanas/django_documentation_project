7fa76f48d051c75c770192bbc3236959ed5c8a28
MeongGanas <farreluken@gmail.com>
2023-07-24 18:51:47 +0800

    menambahkan slug

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,20 @@ from django.urls import path
 
 from . import views
 
+
+app_name = "polls"
 urlpatterns = [
+    # ex: /polls/
     path("", views.index, name="index"),
+
+    # ex: /polls/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+
+    # ex: /polls/5/result/
+    path("<int:question_id>/results/", views.result, name="result"),
+
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote")
+
+    #  <int:question_id>. Menggunakan kurung sudut "menangkap" bagian dari URL dan mengirimnya sebagai argumen kata kunci ke fungsi tampilan. Titik dua (:) memisahkan perubah dan pola nama.
 ]
