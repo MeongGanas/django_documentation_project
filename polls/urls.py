ae2bfb148b43a9d63de1ccc3656a799766aa06b5
MeongGanas <farreluken@gmail.com>
2023-07-25 20:14:44 +0800

    menambah fitur resutls dan votes

--- polls/urls.py
+++ polls/urls.py
@@ -6,16 +6,16 @@ from . import views
 app_name = "polls"
 urlpatterns = [
     # ex: /polls/
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
 
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
 
     # ex: /polls/5/result/
-    path("<int:question_id>/results/", views.result, name="result"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="result"),
 
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote")
 
-    #  <int:question_id>. Menggunakan kurung sudut "menangkap" bagian dari URL dan mengirimnya sebagai argumen kata kunci ke fungsi tampilan. Titik dua (:) memisahkan koverter dan pola nama.
+    #  <int:pk>. Menggunakan kurung sudut "menangkap" bagian dari URL dan mengirimnya sebagai argumen kata kunci ke fungsi tampilan. Titik dua (:) memisahkan koverter dan pola nama.
 ]
